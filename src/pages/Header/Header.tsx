cf1221cc17b81f3173034517134806d8074e5ad1
Nikita <nikita.kuzko2013@gmail.com>
2024-08-19 00:21:18 +0300

    add burger menu

--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -1,43 +1,57 @@
 import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
 import NetflixLogo from '../../assets/images/netflix-logo.png';
+import "./Header.css";
 
 function Header() {
   const location = useLocation();
 
   const activeClass = ( path:string ) => {
     return location.pathname === path
-    ? "text-lg font-semibold text-white text-orange-500"
+    ? "text-lg font-semibold text-orange-500"
     : "text-lg font-semibold text-white hover:text-orange-500"
-  }
+  };
+
+  const links = [
+    {name: 'Home', path: '/'},
+    {name: 'Search', path: '/search'},
+    {name: 'Login', path: '/login'},
+    {name: 'User', path: '/user'},
+  ];
+
+  const [open, setOpen] = useState(false);
 
   return (
-    <header className="bg-transparent p-6 flex justify-between fixed top-0 left-0 w-full">
-      
+    // <header className="bg-transparent p-6 flex justify-between fixed top-0 left-0 w-full header">
+    <header className="md:flex md:justify-between z-20 md:items-center bg-transparent text-center p-6 fixed top-0 left-0 w-full relative">
       <div className="">
         <Link to="/">
           <img src={NetflixLogo} alt="Netflix Logo" />
         </Link>
       </div>
-      <div>
+      <div 
+        className={`burger-icon ${open ? 'active' : ''}`}
+        onClick={() => setOpen(!open)}
+      >
         <span></span>
       </div>
       <nav>
-        <ul className="flex justify-center space-x-6">
-          <li className="transform hover:scale-110 transition duration-200">
-            <Link className={activeClass('/')} to="/">Home</Link>
-          </li>
-          <li className="transform hover:scale-110 transition duration-200">
-            <Link className={activeClass('/search')} to="/search">Search</Link>
-          </li>
-          <li className="transform hover:scale-110 transition duration-200">
-            <Link className={activeClass('/login')} to="/login">login</Link>
-          </li>
-          <li className="transform hover:scale-110 transition duration-200">
-            <Link className={activeClass('/user')} to="/user">User</Link>
-          </li>
+        <ul className={`md:flex gap-5 bg-slate-900 md:bg-transparent md:z-auto absolute w-full md:static z-[-1] ease-in ${open ? '' : '-translate-y-[500px]'} md:translate-y-0`}>
+          {
+            links.map((link) => (
+              <li className="transform p-4 md:p-0 hover:scale-110 transition duration-200">
+                <Link 
+                  className={activeClass(`${link.path}`)} 
+                  to={link.path}
+                  onClick={() => setOpen(!open)}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))
+          }
         </ul>
       </nav>
-      
     </header>
   );
 }
